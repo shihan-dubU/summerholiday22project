8ddff06a1ad33dd2fe5a51ee68d31624ea9a0e0e
SH dubU <wshihan@u.nus.edu>
2022-05-21 19:14:28 +0800

    Completed Shooting Related Stuff

--- Summer Project 22/Assets/Scripts/Player/Player.cs
+++ Summer Project 22/Assets/Scripts/Player/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
     private float movementX;
     private float movementY;
+    private float facingX = 1; // 1 and -1 represents left and right but i cant really tell which is which
     //components:
 
     private Rigidbody2D myBody;
@@ -21,7 +22,7 @@ public class Player : MonoBehaviour
 
     private string SHOOT_ANIMATION = "Shoot";
 
-    private bool notShooting;
+    private bool isShooting;
 
     private void Awake()
     {
@@ -39,12 +40,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerShoot();
         PlayerMovement();
         AnimatePlayer();
     }
 
     void PlayerMovement()
     {
+        //if shooting you cannot move.
+        if (isShooting == true)
+        {
+            return;
+        }
+
         movementX = Input.GetAxis("Horizontal");
         movementY = Input.GetAxis("Vertical");
 
@@ -58,23 +66,34 @@ public class Player : MonoBehaviour
 
     void AnimatePlayer()
     {
-        //cannot move while shooting
-        if (notShooting == true)
+        //if shooting you cannot move.
+        if (isShooting == true)
         {
             return;
         }
 
+        //MOVEMENT
         //going towards right
         if(movementX > 0)
         {
+            if (facingX < 0) //if you are facing to the left, rotate you to the right, set facing to right
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingX = 1f;
+            }
             anim.SetBool(WALK_ANIMATION, true);
-            sr.flipX = false;
+            //sr.flipX = false;
         }
         //going towards left
         else if (movementX < 0)
         {
+            if (facingX > 0) // if you are facing the right, rotate you to the left, set facing to left
+            {
+                transform.Rotate(0f, 180f, 0f);
+                facingX = -1f;
+            }
             anim.SetBool(WALK_ANIMATION, true);
-            sr.flipX = true;
+            //sr.flipX = true;
         }
         //moving up or down
         else if (movementY != 0)
@@ -88,5 +107,29 @@ public class Player : MonoBehaviour
         }
     }
 
+    void PlayerShoot()
+    {
+        if (Input.GetButtonDown("Fire1") && anim.GetBool(WALK_ANIMATION) == false)
+        {
+            Debug.Log("PlayerShoot called");
+            isShooting = true;
+            anim.SetBool(SHOOT_ANIMATION, true);
+        }
+    }
+
+    void ShootComplete()
+    {
+        Debug.Log("Shoot Complete!");
+        isShooting = false;
+        anim.SetBool(SHOOT_ANIMATION, false);
+    }
+
     //TO DO: add shooting mechanics
+
+    //this method is to be called during the shooting animation.
+    void fireWeapon()
+    {
+        Debug.Log("Fire Projectile!");
+        //over here you can do smth like get weapon, call shoot method
+    }
 }
